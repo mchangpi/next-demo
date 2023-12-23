26389887aba2cb05f2aaef39117160876ccb0a3d
Milton Chang <mchangpi@gmail.com>
2023-12-23 23:21:15 +0800

    L10 Adding Images in Next JS

--- 2-get-started/src/app/page.tsx
+++ 2-get-started/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+// import homeImg from "../../public/images/home.jpg";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 text-3xl">
+    <div className="min-h-screen p-24 text-3xl">
       Home Page
+      <Image
+        src={"/images/home.jpg"}
+        alt="car factory"
+        fill
+        style={{ objectFit: "cover" }}
+      />
     </div>
   );
 }
