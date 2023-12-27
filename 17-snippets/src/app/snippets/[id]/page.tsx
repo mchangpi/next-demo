9170cd4144a091df991b2a61e9be430501ca4f33
Milton Chang <mchangpi@gmail.com>
2023-12-27 22:33:55 +0800

    L31 Styling the Show Page

--- 17-snippets/src/app/snippets/[id]/page.tsx
+++ 17-snippets/src/app/snippets/[id]/page.tsx
@@ -17,5 +17,20 @@ export default async function SnippetShowPage({ params }: ShowPageProps) {
   });
   //   console.log(snippet);
 
-  return snippet ? <div>Show snippet: {snippet.title}</div> : notFound();
+  return snippet ? (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">Show snippet: {snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  ) : (
+    notFound()
+  );
 }
