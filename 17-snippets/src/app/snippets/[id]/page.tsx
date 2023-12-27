187bf24ebde8b8e6fa9e47514144fc2f9c3c537e
Milton Chang <mchangpi@gmail.com>
2023-12-27 10:21:35 +0800

    L27 Fetching Particular Records

--- 17-snippets/src/app/snippets/[id]/page.tsx
+++ 17-snippets/src/app/snippets/[id]/page.tsx
@@ -1,4 +1,16 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props);
-  return <div>Show snippet</div>;
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
+interface ShowPageProps {
+  params: { id: string };
+}
+
+export default async function SnippetShowPage({ params }: ShowPageProps) {
+  /* prisma syntax */
+  const snippet = await db.snippet.findFirst({
+    where: { id: Number(params.id) },
+  });
+  //   console.log(snippet);
+
+  return snippet ? <div>Show snippet: {snippet.title}</div> : notFound();
 }
