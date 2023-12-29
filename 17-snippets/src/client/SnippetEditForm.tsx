0cdc1a38af4ac26fc597532dc1fce64e8f0b55f6
Milton Chang <mchangpi@gmail.com>
2023-12-29 17:17:42 +0800

    L39 Calling a Server Action from a Client Component

--- 17-snippets/src/client/SnippetEditForm.tsx
+++ 17-snippets/src/client/SnippetEditForm.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as serverActions from "@/actions";
 
 interface FormProps {
   snippet: Snippet;
@@ -10,10 +11,18 @@ interface FormProps {
 
 function SnippetEditForm({ snippet }: FormProps) {
   const [code, setCode] = useState(snippet.code);
+
   const handleEditorChange = (value: string = "") => {
     // console.log(value);
     setCode(value);
   };
+
+  const handleEditSnippetAction = serverActions.editSnippet.bind(
+    null,
+    snippet.id,
+    code
+  );
+
   return (
     <div>
       {/* {snippet.title} */}
@@ -25,6 +34,11 @@ function SnippetEditForm({ snippet }: FormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={handleEditSnippetAction}>
+        <button type="submit" className="p-2 border-2 rounded m-2">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
