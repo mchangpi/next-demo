d88a2dc0ca396d2e13e3e58535b34f3f704b84c9
Milton Chang <mchangpi@gmail.com>
2024-01-06 22:26:16 +0800

    L73 Creating Topics

--- 52-discuss/src/app/page.tsx
+++ 52-discuss/src/app/page.tsx
@@ -1,15 +1,25 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
+import TopicCreateForm from "@/components/topics/topic-create-form";
 
 export default async function Home() {
-  // const session = await auth();
-
-  return <div>Home Page</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 }
 
-function validateSignInOut(session: any) {
+/*
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import Profile from "@/components/profile";
+
+function validateSignInOut() {
+  const session = await auth();
   console.log("Sign in from Server Component: session", session);
   return (
     <div>
@@ -30,3 +40,4 @@ function validateSignInOut(session: any) {
     </div>
   );
 }
+*/
---
+++ 52-discuss/src/components/topics/topic-create-form.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function TopicCreateForm({}: Props) {
+  return <div>topic-create-form</div>;
+}
