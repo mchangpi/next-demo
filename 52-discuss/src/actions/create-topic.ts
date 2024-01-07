da18052dcde76c549a7b82265c7f30edb90b543f
Milton Chang <mchangpi@gmail.com>
2024-01-07 08:34:22 +0800

    L82 Handling Database Errors in Forms

--- 52-discuss/src/actions/create-topic.ts
+++ 52-discuss/src/actions/create-topic.ts
@@ -1,7 +1,13 @@
 "use server";
 
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
 import { z } from "zod";
 import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
 
 const createTopicSchema = z.object({
   name: z
@@ -26,19 +32,50 @@ export async function createTopic(
   const name = formData.get("name");
   const description = formData.get("description");
 
-  /* revalidate the homepage */
   const result = createTopicSchema.safeParse({ name, description });
-
-  let errors = {};
-  if (!result.success) {
-    errors = result.error.flatten().fieldErrors;
-  }
 
   const session = await auth();
   if (!session || !session.user) {
-    errors = { _form: ["You must sign in to do this"] };
+    return { errors: { _form: ["You must sign in to do this"] } };
   }
 
-  console.log(errors);
-  return { errors };
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  let topic: Topic;
+  try {
+    // throw new Error("Failed to create topic");
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: { _form: [err.message] },
+      };
+    } else {
+      return {
+        errors: { _form: ["Something went wrong"] },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
+
+/*
+model Topic {
+  id          String @id @default(cuid())
+  slug        String @unique
+  description String
+  posts       Post[]
+
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+}
+*/
