babf9ab26b7e4cfb5ba9fc27b32dcf9e4f4a0bcb
Milton Chang <mchangpi@gmail.com>
2024-01-08 07:22:11 +0800

    L86 Displaying a Post Creating Form

--- 52-discuss/src/app/topics/[slug]/page.tsx
+++ 52-discuss/src/app/topics/[slug]/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
 
-type Props = {};
+import PostCreateForm from "@/components/posts/post-create-form";
 
-export default function TopicShowPage({}: Props) {
-  return <div>Topic Show</div>;
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1>{slug}</h1>
+      </div>
+      <div>
+        <PostCreateForm />
+      </div>
+    </div>
+  );
 }
---
+++ 52-discuss/src/components/posts/post-create-form.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useFormState } from "react-dom";
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+
+import * as actions from "@/actions";
+import FormButton from "@/components/common/form-button";
+
+export default function PostCreateForm() {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button color="primary">Create a Post</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
+            <Input
+              name="title"
+              label="Title"
+              labelPlacement="outside"
+              placeholder="Title"
+            />
+            <Input
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
+
+            <FormButton>Create a Post</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
