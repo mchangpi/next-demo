91b4ce5834811f529fc24d1536342e6fe942ac3c
Milton Chang <mchangpi@gmail.com>
2024-01-11 21:32:27 +0800

    L158 Catching More Errors

--- 154-fetchjson/index.ts
+++ 154-fetchjson/index.ts
@@ -2,6 +2,23 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: any;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  console.log(todo.id, todo.title, todo.completed);
+  logTodo(todo.id, todo.title, todo.completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Completed: ${completed} 
+  `);
+};
