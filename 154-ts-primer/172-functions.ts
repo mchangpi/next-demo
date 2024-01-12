be9732672d8758bc58bf54db1945dce99f96cbe4
Milton Chang <mchangpi@gmail.com>
2024-01-13 03:22:35 +0800

    L174 Annotations for Anonymous Functions

--- 154-ts-primer/172-functions.ts
+++ 154-ts-primer/172-functions.ts
@@ -1,6 +1,15 @@
 const add = (a: number, b: number): number => a + b;
 
+/* Always add annotations to the type of RETURN value */
 const subtract = (a: number, b: number): number => {
   // a - b;
   return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiple = function (a: number, b: number): number {
+  return a * b;
 };
