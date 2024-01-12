17353a115bdb193c2c861c3bfc2091d0196e1c39
Milton Chang <mchangpi@gmail.com>
2024-01-13 06:29:14 +0800

    L187 Fixing Annotations with Interfaces

---
+++ 154-ts-primer/187-interfaces.ts
@@ -0,0 +1,20 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+  const { name, year, broken } = vehicle;
+  console.log("Name", name);
+  console.log("Year", year);
+  console.log("Broken?", broken);
+};
+
+console.log(printVehicle);
