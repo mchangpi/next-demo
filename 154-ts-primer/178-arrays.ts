3d09ac92a4a0e5c9d6afcf41a764214f9f4e87d3
Milton Chang <mchangpi@gmail.com>
2024-01-13 05:05:35 +0800

    L180 Multiple Types in Arrays

--- 154-ts-primer/178-arrays.ts
+++ 154-ts-primer/178-arrays.ts
@@ -2,3 +2,21 @@ const carMakers: string[] = ["ford", "toyota", "chevy"];
 const dates: Date[] = [new Date(), new Date()];
 
 const carsByMake: string[][] = [["f150"], ["corolla"], ["camero"]];
+
+// Help with inferemce when extracting values
+const myCar = carMakers.pop();
+console.log("my car", myCar);
+
+// Prevent incompatible values
+// carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((maker: string): string => {
+  return maker.toLocaleUpperCase() + "make car";
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), "2030-10-10"];
+importantDates.push("2040-10-10");
+// importantDates.push(100);
+console.log(importantDates);
